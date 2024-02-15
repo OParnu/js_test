29b28ea4aae6a57cfdd6356e5ef771e301013307
OParnu <110712221+OParnu@users.noreply.github.com>
2024-02-15 12:04:10 +0700

    Create noted2.js

---
+++ noted2.js
@@ -0,0 +1,9 @@
+// document.getElementById - Single Node
+// document.getElementsByClassName - HTMLCollection
+// document.getElementsByTagName - HTMLCollection
+// document.querySelector - First match Single Node
+// document.querySelectorAll -All match (NodeList Collection)
+// children - get child element node types (HTMLCollection)
+// childNodes - get child node (every node type) (NodeList)
+// Array Like - NodeList implement forEach
+// Array Like - HTMLCollection does not implement forEach, using Array.from(HTMLColloction) when use forEach
